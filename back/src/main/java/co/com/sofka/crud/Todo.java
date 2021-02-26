8336fd775a17b98ae9a0e86fad0494f33b208085
jjaramga <juanfelipejg@gmail.com>
2021-02-26 11:25:58 -0500

    Creación de la entidad List y relación con Todo

---
+++ back/src/main/java/co/com/sofka/crud/Todo.java
@@ -0,0 +1,46 @@
+package co.com.sofka.crud;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private boolean completed;
+    @ManyToOne
+    private List groupListId;
+
+
+    public List getGroupListId() {return groupListId;}
+
+    public void setGroupListId(List groupListId) {this.groupListId = groupListId;}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
